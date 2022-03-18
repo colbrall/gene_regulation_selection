ca0ca3dbd5c657f1880c9155eb21e09b5e46ab3c
Laura Colbran <colbranl@pennmedicine.upenn.edu>
2022-03-18 14:51:51 -0400

    added grouping by tissue

--- bin/gene_heatmap.jl
+++ bin/gene_heatmap.jl
@@ -9,6 +9,10 @@ using Seaborn
 
 
 Seaborn.set(style="white", palette="muted")
+COL_MAP = "coolwarm"
+GROUP_SIZES  = [22,14,5,1,7]
+COLOURS = ["#b93e0f","#a1c6d8","#5f3912","#feae02","#6b878f","#c6d737","#171b17","#fe8515","#b7c1c2","#9a5611"]
+
 # ENV["GKSwstype"] = "100" #fixes a segfault bug in GR backend for Plots.jl: https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988
 
 function parseCommandLine()
@@ -65,10 +69,13 @@ function parseCommandLine()
             help = "list of matplotlib colors for the tissue axis labels."
             arg_type = String
             nargs='*'
-            default = vcat(repeat(["#b93e0f"],22),vcat(repeat(["#a1c6d8"],14),
-                vcat(repeat(["#5f3912"],5),vcat(repeat(["#feae02"],1),repeat(["#6b878f"],7)))))
+            default = vcat(repeat([COLOURS[1]],GROUP_SIZES[1]),vcat(repeat([COLOURS[2]],GROUP_SIZES[2]),
+                vcat(repeat([COLOURS[3]],GROUP_SIZES[3]),vcat(repeat([COLOURS[4]],GROUP_SIZES[4]),repeat([COLOURS[5]],GROUP_SIZES[5])))))
         "--scale","-s"
             help = "to scale by expression amount"
+            action=:store_true
+        "--group"
+            help = "if you want to summarize heatmap by groups of tissues"
             action=:store_true
         "--expression","-e"
             help = "only if scaling. path to expression matrix to scale by. Assume GCT file"
@@ -114,40 +121,6 @@ function pullInds(d::Dict{SubString,Array{SubString,1}},l::Array{SubString{Strin
     return dict
 end
 
-# #scales heatmap values by median expression as read from GCT file
-# function scaleByExpr(mat::Array{Float64,2},gene::String,tissues::Array{String,1},expr_path::String)
-#     tiss_cols = String[]
-#     meds = Float64[]
-#     GZip.open(expr_path) do inf
-#         for line in eachline(inf)
-#             if startswith(line,"Name")
-#                 tiss_cols = split(chomp(line),'\t')[3:end] # pull tissues to match indices
-#             elseif startswith(line,gene)
-#                 meds = parse.(Float64,split(chomp(line),'\t')[3:end]) # pull line for medians
-#                 break
-#             end
-#         end
-#     end
-#     # convert tissue names to match what the heatmap uses
-#     for i in 1:length(tiss_cols)
-#         tiss_cols[i] = replace(tiss_cols[i]," - "=>"_")
-#         tiss_cols[i] = replace(tiss_cols[i]," "=>"_")
-#         tiss_cols[i] = replace(tiss_cols[i],"("=>"")
-#         tiss_cols[i] = replace(tiss_cols[i],")"=>"")
-#         tiss_cols[i] = "JTI_$(tiss_cols[i])"
-#     end
-#     # println(tiss_cols)
-#     #for each tissue in matrix, multiply all values by the median.
-#     for (i,tiss) in enumerate(tissues)
-#         # println(tiss)
-#         # println(meds[findfirst(x->x==tiss,tiss_cols)])
-#         # println(mat[i,1:size(mat)[2]])
-#         mat[i,1:size(mat)[2]] = mat[i,1:size(mat)[2]] .* meds[findfirst(x->x==tiss,tiss_cols)]
-#         # println(mat[i,1:size(mat)[2]])
-#     end
-#     return mat
-# end
-
 # scale alpha by expression
 function calcAlphas(gene_id::String,tissues::Array{String,1},expr_path::String,mask_inds::Array{Int64,1})
     # println(mask_inds)
@@ -173,24 +146,19 @@ function calcAlphas(gene_id::String,tissues::Array{String,1},expr_path::String,m
         tiss_cols[i] = "JTI_$(tiss_cols[i])"
     end
     #for each tissue, calculate the alpha
-    # println(tiss_cols)
-    # println(meds)
-    # meds = meds[findall(x->in(x,tissues),tiss_cols)]
-    # println(meds)
     alphas = repeat([1.0],length(tissues))
     for (i,tiss) in enumerate(tissues)
         alphas[i] = meds[findfirst(x->x==tiss,tiss_cols)]
     end
     alphas[mask_inds] .= -100.00
     max_med = maximum(alphas)
-    # println(max_med)
     alphas = round.(alphas./max_med;digits=2)
-    # println(alphas)
     return alphas
 end
 
 function geneHeatMap(qx_paths::Array{String,1},samp_path::String,tiss_order::Array{String,1},pop_order::Array{String,1},
-        gene_id::String,pop_label_colors::Array{String,1},tiss_label_colors::Array{String,1},scale::Bool,expr_file::String)
+        gene_id::String,pop_label_colors::Array{String,1},tiss_label_colors::Array{String,1},scale::Bool,expr_file::String,
+        group::Bool)
     groups = sampDict(samp_path,pop_order) # group -> sample IDs
     samples = String[]
     tissDict = Dict{String,Array{Float64,1}}() #to hold predictions
@@ -225,27 +193,91 @@ function geneHeatMap(qx_paths::Array{String,1},samp_path::String,tiss_order::Arr
             end
         end
     end
-    mask_inds = repeat([true],length(tiss_order),length(pop_order))
     alphas = repeat([1.0],length(tiss_order))
     if scale
         alphas = calcAlphas(gene_id,tiss_order,expr_file,findall(x->x==-100,mat[:,1])) #sorted by tiss_order
     end
+    # mat = [0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         1.0 1 1 1 1.8 1.7 1.6 1 1 1 1 -2.0 1 1.1 1.4 -1.1 1.5 1.7 1.9 1 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 0 0 0 0.8 0.7 0.6 0 0 0 0 -2.0 0 0.1 0.4 -0.1 0.5 0.7 0.9 0 2.1 3.2 0.1 2.2 1.6 2.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3;
+    #         0.0 3.2 1.2 2.2 0.8 0.7 0.6 -0.6 -0.5 -1.0 -1.6 -2.0 -1.2 0.1 0.4 -0.1 0.5 0.7 0.9 -0.9 2.1 3.2 0.1 0.2 0.6 0.3]
+    # alphas = [1.0,0.5,0.1,0.1,0.2,0.4,0.3,0.2,0.1,0.7,0.6,0.5,0.2,0.1,0.5,0.1,0.8,0.4,0.2,0.6,0.2,
+    #                 0.5,0.8,0.5,0.6,0.2,0.8,0.7,0.6,0.5,0.2,0.3,0.4,0.2,0.5,0.6,0.7,0.4,0.3,0.2,0.1,
+    #                 0.7,1.0,1.0,0.5,0.3,0.2,0.1,0.4]
+    # gene_id = "test"
+    if group
+        new_mat = zeros(Float64,length(GROUP_SIZES),length(pop_order))
+        new_alphas = repeat([1.0],length(GROUP_SIZES))
+        new_cols = repeat([COLOURS[1]],length(GROUP_SIZES))
+        tiss_order = ["Group$(i)" for i in 1:length(GROUP_SIZES)]
+        start_ind = 1
+        for grp in 1:length(GROUP_SIZES)
+            tmp = mat[start_ind:start_ind+GROUP_SIZES[grp]-1,:]
+            new_mat[grp,:] =  [mean(tmp[findall(x->x!=-100.0,tmp[:,pop]),pop]) for pop in 1:length(pop_order)] #avg prediction
+            new_cols[grp] = tiss_label_colors[start_ind]
+            if scale
+                new_alphas[grp] = maximum(alphas[start_ind:start_ind+GROUP_SIZES[grp]-1]) #pick largest alpha for display
+            end
+            start_ind = start_ind+GROUP_SIZES[grp]
+        end
+        mat = new_mat
+        alphas = new_alphas
+        tiss_label_colors = new_cols
+    end
+    mask_inds = repeat([true],length(tiss_order),length(pop_order))
     # parameters for heatmap
     color_max = maximum(mat)
     color_min = minimum(mat[findall(x->x!=-100.0,mat)])
     cent = color_min + (color_max-color_min)/2.0
     # first, plot an empty heatmap with the right axis labels, cbar, etc.
-    if length(tiss_order) < length(pop_order)
-        mat = transpose(mat)
-        mask_inds = transpose(mask_inds)
-        h = Seaborn.heatmap(mat,xticklabels=tiss_order,yticklabels=pop_order,square=true,cmap="viridis",mask=mask_inds,vmin=color_min,vmax=color_max,center=cent)
-        [t.set_color(i) for (i,t) in zip(pop_label_colors,h.yaxis.get_ticklabels())]
-        [t.set_color(i) for (i,t) in zip(tiss_label_colors,h.xaxis.get_ticklabels())]
-    else
-        h = Seaborn.heatmap(mat,xticklabels=pop_order,yticklabels=tiss_order,square=true,cmap="viridis",mask=mask_inds,vmin=color_min,vmax=color_max,center=cent)
-        [t.set_color(i) for (i,t) in zip(pop_label_colors,h.xaxis.get_ticklabels())]
-        [t.set_color(i) for (i,t) in zip(tiss_label_colors,h.yaxis.get_ticklabels())]
-    end
+    h = Seaborn.heatmap(mat,xticklabels=pop_order,yticklabels=tiss_order,square=true,cmap=COL_MAP,mask=mask_inds,vmin=color_min,vmax=color_max,center=cent)
+    [t.set_color(i) for (i,t) in zip(pop_label_colors,h.xaxis.get_ticklabels())]
+    [t.set_color(i) for (i,t) in zip(tiss_label_colors,h.yaxis.get_ticklabels())]
     h.tick_params(axis="x",labelsize="4")
     h.tick_params(axis="y",labelsize="4")
     ylabel("Tissue")
@@ -256,15 +288,9 @@ function geneHeatMap(qx_paths::Array{String,1},samp_path::String,tiss_order::Arr
         mask_inds = repeat([false],length(tiss_order),length(pop_order)) # clear previous mask
         mask_inds[findall(x->x==-100.0,mat)] .= true #mask missing tissues
 
-        if length(tiss_order) < length(pop_order)
-            mask_inds[:,findall(x->x!=alph,alphas)] .= true #mask tissues with a different alpha
-            h = Seaborn.heatmap(mat,xticklabels=tiss_order,yticklabels=pop_order,square=true,cmap="viridis",alpha=alph,
-                mask=mask_inds,vmin=color_min,vmax=color_max,center=cent,cbar = false) #xticklabels = false,yticklabels = false,
-        else
-            mask_inds[findall(x->x!=alph,alphas),:] .= true #mask tissues with a different alpha
-            h = Seaborn.heatmap(mat,xticklabels=pop_order,yticklabels=tiss_order,square=true,cmap="viridis",alpha=alph,
-                mask=mask_inds,vmin=color_min,vmax=color_max,center=cent,cbar = false)
-        end
+        mask_inds[findall(x->x!=alph,alphas),:] .= true #mask tissues with a different alpha
+        h = Seaborn.heatmap(mat,xticklabels=pop_order,yticklabels=tiss_order,square=true,cmap=COL_MAP,alpha=alph,
+            mask=mask_inds,vmin=color_min,vmax=color_max,center=cent,cbar = false)
     end
     Seaborn.savefig("$(gene_id)_qx_heatmap.pdf")
     clf()
@@ -272,7 +298,7 @@ end
 
 function main()
     parsed_args = parseCommandLine()
-    geneHeatMap(parsed_args["qx"],parsed_args["populations"],parsed_args["tiss_order"],parsed_args["pop_order"],parsed_args["genes"],parsed_args["pop_colors"],parsed_args["tiss_colors"],parsed_args["scale"],parsed_args["expression"])
+    geneHeatMap(parsed_args["qx"],parsed_args["populations"],parsed_args["tiss_order"],parsed_args["pop_order"],parsed_args["genes"],parsed_args["pop_colors"],parsed_args["tiss_colors"],parsed_args["scale"],parsed_args["expression"],parsed_args["group"])
 end
 
 main()
