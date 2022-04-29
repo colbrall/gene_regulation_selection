a463c0182ee4773bea718184592ab629894ea9db
Laura Colbran <colbranl@pennmedicine.upenn.edu>
2022-04-29 15:57:33 -0400

    streamlined and added empirical p-value calculation

--- bin/qx.jl
+++ bin/qx.jl
@@ -9,15 +9,15 @@
 
 
 using ArgParse
-using SQLite
-using GZip
+using Dates,StatsBase
+using SQLite,GZip,DelimitedFiles
 
 SNP_COL = :rsid #saving variable model DB uses
 GENE_COL = :gene #saving variable model DB uses
 WEIGHT_COL = :weight #saving variable model DB uses
 REF_COL = :ref_allele
 ALT_COL = :eff_allele
-DBSNP_FILE = "/project/mathilab/colbranl/gene_regulation_selection/data/jti_snp_coordinates.txt.gz"
+DBSNP_FILE = "/project/mathilab/colbranl/data/jti_snp_coordinates.txt.gz"
 
 function parseCommandLine()
     s = ArgParseSettings()
@@ -44,6 +44,13 @@ function parseCommandLine()
             help = "number of genes to run per bsub job"
             arg_type = Int64
             default = 100
+        "--pvalue"
+            help = "if you want to calculate p-value by resampling effect sizes"
+            action=:store_true
+        "--resample_n","-r"
+            help = "number of times to resample for p-value"
+            default = 100
+            arg_type = Int64
     end
     return parse_args(s)
 end
@@ -81,6 +88,7 @@ end
 # read in database, then for each gene pull snps and effect sizes
 function parseDB(path::String)
     weights = Dict{String,Array{Tuple{String,Float64,String,String},1}}() # Dict{gene => [(id,weight)]}
+
     # open SQLite connection, read weights table
     q = "SELECT * FROM 'weights'"
     for snp in DBInterface.execute(SQLite.DB(path),q)
@@ -90,16 +98,90 @@ function parseDB(path::String)
             weights[snp[GENE_COL]] = vcat(weights[snp[GENE_COL]],[(snp[SNP_COL],snp[WEIGHT_COL],snp[REF_COL],snp[ALT_COL])])
         end
     end
-    return weights
+    return weights,[snp[2] for gene in keys(weights) for snp in weights[gene]]
 end
 
-function runBSUB(outdir::String,commands::Array{String,1},n::Int64,genes_per_job::Int64)
-    bsub_path = "$(outdir)genes$(n-(genes_per_job)+1)-$(n).bsub"
+#reads file with matched SNPs for each gene (output of matchSNPs())
+function readMatch(path::String,gene::String)
+    snps = String[]
+    found_gene = false
+    open(path) do inf
+        for line in eachline(inf)
+            if startswith(line,gene)
+                found_gene = true
+                snps = vcat(snps,"$(split(line,"\t")[2])")
+                continue
+            end
+            if found_gene break end #keeps from reading whole file if it doesn't have to
+        end
+    end
+    return snps
+end
+
+#pulls population freqencies for SNPs
+function pullFreqs(snps::Array{String,1},frq_path::String,pop_tags::Array{String,1})
+    # initiate fqcy matrix
+    freqs = zeros(Float64,length(pop_tags),length(snps))
+    chrs = [split(split(snp,"_")[1],"r")[2] for snp in snps]
+    positions = [join(split(snp,"_")[1:2],"_") for snp in snps]
+    alts = [split(snp,"_")[4] for snp in snps]
+    #build tabix regions file
+    tmp_file = "tmp.regions.$(Dates.now())" #naming so that parallel jobs don't collide
+    open(tmp_file,"w") do outf
+        for ind in 1:length(snps)
+            write(outf,"$(replace(positions[ind],'_'=>'\t'))\n")
+        end
+    end
+    for chr in unique(chrs)
+        chr_path = replace(frq_path,"*" => chr)
+        # id indices for pops
+        pop_inds = Int64[]
+        GZip.open(chr_path) do chrf
+            for line in eachline(chrf)
+                l = split(chomp(line),"\t")
+                if startswith(l[1],"CHROM")
+                    for pop in pop_tags
+                        pop_inds = vcat(pop_inds,findfirst([isequal(pop,x) for x in l]))
+                    end
+                    break
+                end
+            end
+        end
+        #query SNPs for tabix
+        command = `tabix -R $(tmp_file) $(chr_path)`
+        open(command) do inf
+            for line in eachline(inf)
+                l = split(chomp(line),"\t")
+                snp_ind = findfirst(isequal("$(l[1])_$(l[2])"),positions)
+                # println(l)
+                # println(snps[snp_ind])
+                allele_ind = findfirst(isequal(alts[snp_ind]),[split(x,":")[1] for x in split(l[pop_inds[1]],",")])
+                # println(allele_ind)
+                if typeof(allele_ind) == Nothing continue end #skip if alt allele isn't present
+                snp_freqs = [split(split(pop,",")[allele_ind],":")[2] for pop in l[pop_inds]]
+                # modify freqs so correct column has snp_freq values
+                freqs[:,snp_ind] = parse.(Float64,snp_freqs)
+            end
+        end
+    end
+    file_rm = `rm $(tmp_file)`
+    run(file_rm)
+    indices = Int64[]
+    for col in 1:size(freqs)[2] #record indices of any SNPs we couldn't find frequencies for, so we can remove them
+        if freqs[:,col] == repeat([0.0],size(freqs)[1])
+            indices = vcat(indices,col)
+        end
+    end
+    return freqs,indices
+end
+
+function runBSUB(outdir::String,commands::Array{String,1},n::Int64,genes_per_job::Int64,name::String)
+    bsub_path = "$(outdir)$(name)$(n-(genes_per_job)+1)-$(n).bsub"
     open(bsub_path,"w") do outf
         write(outf,"#!/bin/bash\n")
         write(outf,"#BSUB -J qx$(n-genes_per_job+1)-$(n)\n")
-        write(outf,"#BSUB -o $(outdir)genes$(n+1-genes_per_job)-$(n).out\n")
-        write(outf,"#BSUB -e $(outdir)genes$(n+1-genes_per_job)-$(n).err\n\n")
+        write(outf,"#BSUB -o $(outdir)$(name)$(n+1-genes_per_job)-$(n).out\n")
+        write(outf,"#BSUB -e $(outdir)$(name)$(n+1-genes_per_job)-$(n).err\n\n")
         for comm in commands
             write(outf,comm)
         end
@@ -108,39 +190,69 @@ function runBSUB(outdir::String,commands::Array{String,1},n::Int64,genes_per_job
 end
 
 #organizes input parsing for Qx calculation
-function QxByGene(db_path::String,match_path::String,freq_path::String,pop_tags::String,genes_per_job::Int64)
+function QxByGene(db_path::String,match_path::String,freq_path::String,pop_tags::Array{String,1},genes_per_job::Int64,pvalue::Bool,res_n::Int64)
     outdir = "$(splitext(db_path)[1])/"
     if !isdir(outdir) mkdir(outdir) end
-    genes = parseDB(db_path) # Dict{gene => [(id,weight)]}
+    genes,eff_sizes = parseDB(db_path) # Dict{gene => [(id,weight)]}
     n = 1
     commands = String[]
     coord_ids = mapIDs(DBSNP_FILE)
+
     for gene in keys(genes)
+        println(gene)
         if n%genes_per_job == 0
-            runBSUB(outdir,commands,n,genes_per_job)
+            runBSUB(outdir,commands,n-1,genes_per_job,"genes")
             commands = String[]
         end
         snp_arr = [snp[1] for snp in genes[gene]]
         if !occursin("_",snp_arr[1]) #if IDs are not in coordinate ID form
             snp_arr = coordinateID(coord_ids,snp_arr,[snp[3] for snp in genes[gene]],[snp[4] for snp in genes[gene]])
         end
-        unmatched = findall(x -> x=="",snp_arr)
-        deleteat!(snp_arr,unmatched)
+        no_id = findall(x -> x=="",snp_arr)
+        deleteat!(snp_arr,no_id)
         snps = join([snp for snp in snp_arr]," ")
-        betas = [snp[2] for snp in genes[gene]]
-        deleteat!(betas,unmatched)
-        command = "julia ./bin/qx_per_gene.jl -g $(gene) -l $(snps) -b $(join(betas,' ')) -o $(outdir)$(gene)_qx.txt -f '$(freq_path)' -m $(match_path) -t $(pop_tags)\n"
-        # println(command)
-        # exit()
+        betas = ["$(snp[2])" for snp in genes[gene]]
+        deleteat!(betas,no_id)
+
+        matched_snps = readMatch(match_path,gene)
+        all_freqs,zero_inds = pullFreqs(vcat(snp_arr,matched_snps),freq_path,pop_tags)
+        all_freqs = all_freqs[:,setdiff(1:end, zero_inds)] #remove all-zero-freq SNPs
+
+        betas = betas[setdiff(1:end,zero_inds[findall(<(length(betas)+1),zero_inds)])]
+
+        #write the freq matrix so that it can be passed to qx_per_gene
+        freq_file = "$(outdir)frqs/tmp.freqs.$(gene)"
+        writedlm(freq_file,all_freqs)
+
+        command = "julia ./bin/qx_per_gene.jl -g $(gene) -o $(outdir)$(gene)_model_qx.txt -b $(join(betas,' ')) -f $(freq_file)\n"
         commands = vcat(commands,[command])
         n+=1
+        if pvalue
+            #resample from eff_sizes and add to command
+            m=1
+            rn=1
+            p_coms = String[]
+            for samp in 1:res_n
+                if n%genes_per_job == 0
+                    m+=1
+                    runBSUB(outdir,p_coms,rn-1,genes_per_job,"$(gene)_rand")
+                    p_coms = String[]
+                end
+                rand_betas = sample(eff_sizes,length(betas))
+                println(rand_betas)
+                command = "julia ./bin/qx_per_gene.jl -g $(gene) -o $(outdir)$(gene)_rand$(m).txt -b $(join(rand_betas,' ')) -f $(freq_file)\n"
+                p_coms = vcat(p_coms,[command])
+                rn += 1
+            end
+            runBSUB(outdir,p_coms,rn,length(p_coms),"$(gene)_rand")
+        end
     end
-    runBSUB(outdir,commands,n,length(commands)) #to catch the last few genes
+    runBSUB(outdir,commands,n-1,length(commands),"genes") #to catch the last few genes
 end
 
 function main()
     parsed_args = parseCommandLine()
-    QxByGene(parsed_args["db"],parsed_args["matched_snps"],parsed_args["pop_freqs"],join(parsed_args["pop_tags"]," "),parsed_args["genes_per_job"])
+    QxByGene(parsed_args["db"],parsed_args["matched_snps"],parsed_args["pop_freqs"],parsed_args["pop_tags"],parsed_args["genes_per_job"],parsed_args["pvalue"],parsed_args["resample_n"])
 end
 
 main()
--- bin/qx_per_gene.jl
+++ bin/qx_per_gene.jl
@@ -5,10 +5,7 @@
 # julia 1.5
 
 using ArgParse
-using Dates
-using GZip
-using StatsBase
-using LinearAlgebra
+using StatsBase,LinearAlgebra
 
 function parseCommandLine()
     s = ArgParseSettings()
@@ -17,23 +14,10 @@ function parseCommandLine()
         "--gene","-g"
             help = "gene ID"
             arg_type = String
-        "--pop_frqs","-f"
-            help = "path to vcfs with population AFs for calculating Qx. Assumes that they are split by chr, and that you've put a * in for the chr number, and that they've been indexed by tabix"
-            arg_type = String
-            required = true
-        "--matched_snps","-m"
+        "--frequencies","-f"
             help = "file with list of IDs of matched SNPs for run. if left empty, script will match SNPs itself using --snps option"
             arg_type =  String
             default = ""
-        "--pop_tags","-t"
-            nargs='*'
-            help = "list of population tags from frq file to pull AFs for. default matches 1kG continental pops"
-            arg_type = String
-            default = ["AFR","AMR","EUR","EAS","SAS"]
-        "--model_snps","-l"
-            nargs='*'
-            help = "list of SNP IDs in model"
-            arg_type = String
         "--betas","-b"
             nargs='*'
             help = "list of betas for SNPs in model"
@@ -43,6 +27,20 @@ function parseCommandLine()
             arg_type = String
     end
     return parse_args(s)
+end
+
+function readFloatDelim(f::String,delim::String)
+   arr=Float64[]
+   open(f) do inf
+       for line in eachline(inf)
+           if length(arr) == 0
+               arr = parse.(Float64,split(chomp(line),delim))
+           else
+               arr = hcat(arr,parse.(Float64,split(chomp(line),delim)))
+           end
+       end
+   end
+   return transpose(arr)
 end
 
 # returns matrix to center and scale Z matrix of AFs
@@ -93,135 +91,27 @@ function Qx(snp_betas::Transpose{Float64,Array{Float64,1}},snp_freqs::Transpose{
     return qx
 end
 
-#reads file with matched SNPs for each gene (output of matchSNPs())
-function readMatch(path::String,gene::String)
-    snps = String[]
-    found_gene = false
-    open(path) do inf
-        for line in eachline(inf)
-            if startswith(line,gene)
-                found_gene = true
-                snps = vcat(snps,"$(split(line,"\t")[2])")
-                continue
-            end
-            if found_gene break end #keeps from reading whole file if it doesn't have to
-        end
-    end
-    return snps
-end
-
-# reads population and ids into a dictionary
-function popDict(path::String)
-    dict = Dict{String,Array{String,1}}() #pop => id list
-    GZip.open(path) do inf
-        for line in eachline(inf)
-            e = split(chomp(line),"\t")
-            if !in("$(e[2])",keys(dict))
-                dict["$(e[2])"] = ["$(e[1])"]
-            else
-                dict["$(e[2])"] = vcat(dict["$(e[2])"],["$(e[1])"])
-            end
-        end
-    end
-    return dict
-end
-
-#pulls population freqencies for SNPs
-function pullFreqs(snps::Array{String,1},frq_path::String,pop_tags::Array{String,1})
-    # initiate fqcy matrix
-    freqs = zeros(Float64,length(pop_tags),length(snps))
-    chrs = [split(split(snp,"_")[1],"r")[2] for snp in snps]
-    positions = [join(split(snp,"_")[1:2],"_") for snp in snps]
-    alts = [split(snp,"_")[4] for snp in snps]
-    #build tabix regions file
-    tmp_file = "tmp.regions.$(Dates.now())" #naming so that parallel jobs don't collide
-    open(tmp_file,"w") do outf
-        for ind in 1:length(snps)
-            write(outf,"$(replace(positions[ind],'_'=>'\t'))\n")
-        end
-    end
-    for chr in unique(chrs)
-        chr_path = replace(frq_path,"*" => chr)
-        # id indices for pops
-        pop_inds = Int64[]
-        # println(chr_path)
-        GZip.open(chr_path) do chrf
-            for line in eachline(chrf)
-                l = split(chomp(line),"\t")
-                if startswith(l[1],"CHROM")
-                    for pop in pop_tags
-                        pop_inds = vcat(pop_inds,findfirst([isequal(pop,x) for x in l]))
-                    end
-                    # println(pop_inds)
-                    break
-                end
-            end
-        end
-        #query SNPs for tabix
-        command = `tabix -R $(tmp_file) $(chr_path)`
-        open(command) do inf
-            for line in eachline(inf)
-                l = split(chomp(line),"\t")
-                snp_ind = findfirst(isequal("$(l[1])_$(l[2])"),positions)
-                # println(l)
-                # println(snps[snp_ind])
-                allele_ind = findfirst(isequal(alts[snp_ind]),[split(x,":")[1] for x in split(l[pop_inds[1]],",")])
-                # println(allele_ind)
-                if typeof(allele_ind) == Nothing continue end #skip if alt allele isn't present
-                snp_freqs = [split(split(pop,",")[allele_ind],":")[2] for pop in l[pop_inds]]
-                # modify freqs so correct column has snp_freq values
-                freqs[:,snp_ind] = parse.(Float64,snp_freqs)
-            end
-        end
-        # println("\tchr$(chr) freqs: $(Dates.now())")
-    end
-    file_rm = `rm $(tmp_file)`
-    run(file_rm)
-    indices = Int64[]
-    for col in 1:size(freqs)[2] #record indices of any SNPs we couldn't find frequencies for, so we can remove them
-        if freqs[:,col] == repeat([0.0],size(freqs)[1])
-            indices = vcat(indices,col)
-        end
-    end
-    # println("\tupdate freqs matrix: $(Dates.now())")
-    return freqs,indices
-end
-
 function main()
     parsed_args = parseCommandLine()
-    println(parsed_args["gene"])
-    # println("Start: $(Dates.now())")
     qx_path = parsed_args["out_path"]
     gene = parsed_args["gene"]
-    num_snps = length(parsed_args["model_snps"])
+    num_snps = length(parsed_args["betas"])
 
-    matched_snps = readMatch(parsed_args["matched_snps"],parsed_args["gene"])
-    # println("Read Matches: $(Dates.now())")
-    # println(size(matched_snps))
-    all_freqs,zero_inds = pullFreqs(vcat(parsed_args["model_snps"],matched_snps),parsed_args["pop_frqs"],parsed_args["pop_tags"])
-    # println(size(all_freqs))
-    # println(all_freqs[1:end,1:10])
 
-    # println("pull all Freqs: $(Dates.now())")
-    snp_freqs = all_freqs[1:end,1:num_snps]
+    snp_betas = transpose(parsed_args["betas"])
+
+    #read in freqs, and transpose the ones for the model snps
+    all_freqs = readFloatDelim(parsed_args["frequencies"],"\t")
+
+    snp_freqs = transpose(all_freqs[1:end,1:num_snps])
     # println(size(snp_freqs))
-    snp_freqs = snp_freqs[:,setdiff(1:end, zero_inds[findall(<(num_snps+1),zero_inds)])] #remove all-zero-freq SNPs
-    # println(size(snp_freqs))
-    # println(size(parsed_args["betas"]))
-    snp_betas = transpose(parsed_args["betas"][setdiff(1:end,zero_inds[findall(<(num_snps+1),zero_inds)])])
-    snp_freqs = transpose(snp_freqs)
-
     matched_freqs = all_freqs[1:end,(num_snps+1):end]
     # println(size(matched_freqs))
-    matched_freqs = matched_freqs[:,setdiff(1:end, zero_inds[findall(>(num_snps),zero_inds)] .- num_snps)]  #remove all-zero-freq SNPs
-    # println(size(matched_freqs))
 
-    # println("Split Freqs: $(Dates.now())")
     qx = Qx(snp_betas,snp_freqs,matched_freqs)
-    open(qx_path,"w") do outf
+    open(qx_path,"a") do outf
         write(outf,"$(gene)\t$(num_snps)\t$qx\n")
     end
-    # println("Finish: $(Dates.now())")
 end
 
 main()
